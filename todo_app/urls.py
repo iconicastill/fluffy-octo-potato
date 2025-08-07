51fc7436b75a9d43aa0cb80dea1c376bafe80705
Nicolas Castillo <121370448+iconicastill@users.noreply.github.com>
2025-08-07 00:51:54 -0400

    Update urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('get-csrf-token/', views.get_csrf_token, name='get_csrf_token'),
 ]
