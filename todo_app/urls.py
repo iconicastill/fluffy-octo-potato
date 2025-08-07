fc4afdcd7156f20548774110175ab423a58f240f
Nicolas Castillo <121370448+iconicastill@users.noreply.github.com>
2025-08-07 01:28:03 -0400

    Update urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # URLs de la app de tareas
     path('', views.index, name='index'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+
+    # Nuevas URLs para el formulario de contacto
     path('get-csrf-token/', views.get_csrf_token, name='get_csrf_token'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
