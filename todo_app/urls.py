07f3e8ab31a5f2fab08d0f4b6e3ead6eaafd7966
Nicolas Castillo <121370448+iconicastill@users.noreply.github.com>
2025-08-12 16:55:56 -0400

    Update urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -14,13 +14,13 @@ urlpatterns = [
     path('get-csrf-token/', views.get_csrf_token, name='get_csrf_token'),
     path('contacto/', views.contacto, name='contacto'),
     
-    # URL para los mensajes públicos (nueva vista)
+    # URL para los mensajes públicos
     path('mensajes/', views.ver_mensajes, name='ver_mensajes'),
     
     # URL de la vista de administración
-    path('admin/mensajes/', views.admin_mensajes, name='admin_mensajes'),
+    path('panel/mensajes/', views.admin_mensajes, name='admin_mensajes'),
     
     # Nuevas URLs para las funcionalidades de administración
-    path('admin/marcar-leido/<int:pk>/', views.marcar_como_leido, name='marcar_como_leido'),
-    path('admin/borrar/<int:pk>/', MensajeDeleteView.as_view(), name='mensaje_borrar'),
+    path('panel/marcar-leido/<int:pk>/', views.marcar_como_leido, name='marcar_como_leido'),
+    path('panel/borrar/<int:pk>/', MensajeDeleteView.as_view(), name='mensaje_borrar'),
 ]
