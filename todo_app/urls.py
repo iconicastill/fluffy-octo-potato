1052591bdd6e1a67481a372a48afa29547235ed4
Nicolas Castillo <121370448+iconicastill@users.noreply.github.com>
2025-08-12 15:30:06 -0400

    Update urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from . import views
-from .views import MensajeDeleteView  # Importa la vista de clase MensajeDeleteView
+from .views import MensajeDeleteView
 
 urlpatterns = [
     # URLs de la app de tareas (sin cambios)
@@ -14,10 +14,13 @@ urlpatterns = [
     path('get-csrf-token/', views.get_csrf_token, name='get_csrf_token'),
     path('contacto/', views.contacto, name='contacto'),
     
-    # URL de la vista de mensajes que ya existe, pero que ahora usaremos para la administración.
-    path('mensajes/', views.admin_mensajes, name='admin_mensajes'),
+    # URL para los mensajes públicos (nueva vista)
+    path('mensajes/', views.ver_mensajes, name='ver_mensajes'),
+    
+    # URL de la vista de administración
+    path('admin/mensajes/', views.admin_mensajes, name='admin_mensajes'),
     
     # Nuevas URLs para las funcionalidades de administración
-    path('mensajes/marcar-leido/<int:pk>/', views.marcar_como_leido, name='marcar_como_leido'),
-    path('mensajes/borrar/<int:pk>/', MensajeDeleteView.as_view(), name='mensaje_borrar'),
+    path('admin/marcar-leido/<int:pk>/', views.marcar_como_leido, name='marcar_como_leido'),
+    path('admin/borrar/<int:pk>/', MensajeDeleteView.as_view(), name='mensaje_borrar'),
 ]
